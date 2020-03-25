7b78ba80049ed30d799bdf630ea85cacee065ccc
harmscorey@gmail.com <harmscorey@gmail.com>
2020-03-25 09:41:20 -0500

    Added config-example to remove passwords and ips, added config to ignore

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var _ = require('lodash');
 var Collector = require('Netflow');
 var sql = require('mssql');
-config = require('./configuration');
+config = require('./config');
 
 var packetCount = 0;
 var packetThreshold = 800; //number of packets before writing to DB
